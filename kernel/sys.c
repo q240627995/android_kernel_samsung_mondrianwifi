29937da1116aec4f66e703aabb3240ccccc6c1ab
Ruchi Kandoi <kandoiruchi@google.com>
2014-04-18 14:07:28 -0700

    prctl: adds PR_SET_TIMERSLACK_PID for setting timer slack of an arbitrary thread.
    Second argument is similar to PR_SET_TIMERSLACK, if non-zero then the
    slack is set to that value otherwise sets it to the default for the thread.
    
    Takes PID of the thread as the third argument.
    
    This allows power/performance management software to set timer slack for
    other threads according to its policy for the thread (such as when the
    thread is designated foreground vs. background activity)
    
    Change-Id: I744d451ff4e60dae69f38f53948ff36c51c14a3f
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>
    
    Conflicts:
    	kernel/sys.c

--- kernel/sys.c
+++ kernel/sys.c
@@ -42,6 +42,7 @@
 #include <linux/ctype.h>
 #include <linux/mm.h>
 #include <linux/mempolicy.h>
+#include <linux/sched.h>
 
 #include <linux/compat.h>
 #include <linux/syscalls.h>
@@ -2072,6 +2073,7 @@ SYSCALL_DEFINE5(prctl, int, option, unsigned long, arg2, unsigned long, arg3,
 		unsigned long, arg4, unsigned long, arg5)
 {
 	struct task_struct *me = current;
+	struct task_struct *tsk;
 	unsigned char comm[sizeof(me->comm)];
 	long error;
 
