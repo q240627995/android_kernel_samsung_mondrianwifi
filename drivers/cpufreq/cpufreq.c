32221c63adcb69477eefe6650923a71caacc3801
Kx <vikrant82@gmail.com>
2014-08-03 20:25:53 +0530

    More tweaks + Zen + Vr + Logging reduce + FS tweaks + Net tweaks
    Change-Id: I7e727ffee281bf8ef058db3ceefa6b3a0b2241cf

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -698,9 +698,9 @@ static struct attribute *default_attrs[] = {
 	&scaling_driver.attr,
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
-	#ifdef CONFIG_MSM_CPU_VOLTAGE_CONTROL
+#ifdef CONFIG_MSM_CPU_VOLTAGE_CONTROL
 	&UV_mV_table.attr,
-	#endif
+#endif
 	NULL
 };
 
