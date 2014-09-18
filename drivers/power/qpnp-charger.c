cb61391174db88a22487e25db8c2e8f05e22dd7c
andip71 <andreasp@gmx.de>
2014-09-18 06:56:59 +0200

    Vikx: Doesn't work properly yet. Have to adapt
    qpnp-charger: Charge level interface
    
    Signed-off-by: Zachariah <zkennedy87@gmail.com>
    
    Conflicts:
    	arch/arm/mach-msm/Makefile
    	drivers/power/qpnp-charger.c
    
    Conflicts:
    	arch/arm/mach-msm/Kconfig
    	arch/arm/mach-msm/Makefile
    	drivers/power/qpnp-charger.c
    
    Change-Id: I0d0049c913880817f5de556838e36348047110fd

--- drivers/power/qpnp-charger.c
+++ drivers/power/qpnp-charger.c
@@ -37,6 +37,14 @@
 #include <linux/fastchg.h>
 #endif
 
+#ifdef CONFIG_CHARGE_LEVEL
+#include "linux/charge_level.h"
+int ac_level = AC_CHARGE_LEVEL_DEFAULT;    // Set AC default charge level
+int usb_level  = USB_CHARGE_LEVEL_DEFAULT; // Set USB default charge level
+char charge_info_text[30];
+int charge_info_level;
+#endif
+
 /* Interrupt offsets */
 #define INT_RT_STS(base)			(base + 0x10)
 #define INT_SET_TYPE(base)			(base + 0x11)
@@ -911,6 +919,7 @@ qpnp_chg_iusbmax_set(struct qpnp_chg_chip *chip, int mA)
 	}
 
 	pr_debug("current=%d setting 0x%x\n", mA, usb_reg);
+
 	rc = qpnp_chg_write(chip, &usb_reg,
 		chip->usb_chgpth_base + CHGR_I_MAX_REG, 1);
 
@@ -5150,6 +5159,11 @@ static struct spmi_driver qpnp_charger_driver = {
 int __init
 qpnp_chg_init(void)
 {
+#ifdef CONFIG_CHARGE_LEVEL
+	// initialize charge info variables
+	charge_info_level = 0;
+	sprintf(charge_info_text, "No charger");
+#endif
 	return spmi_driver_register(&qpnp_charger_driver);
 }
 module_init(qpnp_chg_init);
