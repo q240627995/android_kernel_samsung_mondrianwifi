21bb36bd6b80db74e29247163f18e921acf35877
Zachariah <zkennedy87@gmail.com>
2015-03-09 02:21:54 -0400

    Bricked Thermal: Initial Port for OPO
    Signed-off-by: Zachariah <zkennedy87@gmail.com>
    
    Conflicts:
    	arch/arm/mach-msm/include/mach/cpufreq.h
    	arch/arm/mach-msm/oppo/board-8974-oppo.c
    
    Change-Id: Ifc888b0654c102866110eced074eda87a9f4c5f6

--- arch/arm/mach-msm/include/mach/cpufreq.h
+++ arch/arm/mach-msm/include/mach/cpufreq.h
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 /*
  * Copyright (c) 2013 The Linux Foundation. All rights reserved.
+=======
+/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
+>>>>>>> 5195904... Bricked Thermal: Initial Port for OPO
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -10,6 +14,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
+<<<<<<< HEAD
 #ifndef __MACH_CPUFREQ_H
 #define __MACH_CPUFREQ_H
 
@@ -37,3 +42,36 @@ extern unsigned long msm_cpufreq_get_bw(void)
 #endif
 
 #endif
+=======
+
+#ifndef __ARCH_ARM_MACH_MSM_MACH_CPUFREQ_H
+#define __ARCH_ARM_MACH_MSM_MACH_CPUFREQ_H
+
+#define MSM_CPUFREQ_NO_LIMIT 0xFFFFFFFF
+
+#ifdef CONFIG_CPU_FREQ_MSM
+
+/**
+ * msm_cpufreq_set_freq_limit() - Set max/min freq limits on cpu
+ *
+ * @cpu: The cpu core for which the limits apply
+ * @max: The max frequency allowed
+ * @min: The min frequency allowed
+ *
+ * If the @max or @min is set to MSM_CPUFREQ_NO_LIMIT, the limit
+ * will default to the CPUFreq limit.
+ *
+ * returns 0 on success, errno on failure
+ */
+extern int msm_cpufreq_set_freq_limits(
+		uint32_t cpu, uint32_t min, uint32_t max);
+#else
+static inline int msm_cpufreq_set_freq_limits(
+		uint32_t cpu, uint32_t min, uint32_t max)
+{
+	return -ENOSYS;
+}
+#endif
+
+#endif /* __ARCH_ARM_MACH_MSM_MACH_CPUFREQ_H */
+>>>>>>> 5195904... Bricked Thermal: Initial Port for OPO
