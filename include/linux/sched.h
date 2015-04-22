0f6e3ff73aa463bc356232a3007c804e053b3c52
Zachariah <zkennedy87@gmail.com>
2015-04-21 21:53:41 -0400

    Missing Bits for State Notifier Driver update
    Signed-off-by: Zachariah <zkennedy87@gmail.com>
    
    Conflicts:
    	include/linux/sched.h
    
    Change-Id: If145174b2bedc7b91b4697e4c370df9321d352b1

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -145,6 +145,10 @@ extern unsigned long this_cpu_load(void);
 #ifdef CONFIG_RUNTIME_COMPCACHE
 extern unsigned long this_cpu_loadx(int i);
 #endif /* CONFIG_RUNTIME_COMPCACHE */
+#ifdef CONFIG_INTELLI_HOTPLUG
+extern unsigned long avg_nr_running(void);
+extern unsigned long avg_cpu_nr_running(unsigned int cpu);
+#endif
 
 extern void sched_update_nr_prod(int cpu, unsigned long nr, bool inc);
 extern void sched_get_nr_running_avg(int *avg, int *iowait_avg);
