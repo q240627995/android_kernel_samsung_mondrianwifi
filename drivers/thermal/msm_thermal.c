8f1bac827f0fa6276c188d0f4b025e9d804b86ad
DerRomtester <dieb.stefan.96@gmail.com>
2015-05-03 15:12:19 +0200

    msm: thermal: add a module param to change the thermal throttle temperature point to userspace
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: DerRomtester <dieb.stefan.96@gmail.com>
    
    Conflicts:
    	drivers/thermal/msm_thermal.c
    
    Change-Id: I8e4b3b98ab63ac758d7d9783291ba93db98cb2b8

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -49,6 +49,9 @@
 #define MAX_EFUSE_VALUE  16
 #define THERM_SECURE_BITE_CMD 8
 
+unsigned int temp_threshold = 70;
+module_param(temp_threshold, int, 0755);
+
 static struct msm_thermal_data msm_thermal_info;
 static struct delayed_work check_temp_work;
 static bool core_control_enabled;
@@ -1295,7 +1298,7 @@ static void __ref do_freq_control(long temp)
 	uint32_t cpu = 0;
 	uint32_t max_freq = cpus[cpu].limited_max_freq;
 
-	if (temp >= msm_thermal_info.limit_temp_degC) {
+	if (temp >= temp_threshold) {
 		if (limit_idx == limit_idx_low)
 			return;
 
@@ -1308,7 +1311,7 @@ static void __ref do_freq_control(long temp)
 		pr_info("%s: down Limit=%d Temp: %ld\n",
 				KBUILD_MODNAME, limit_idx, temp);
 #endif
-	} else if (temp < msm_thermal_info.limit_temp_degC -
+	} else if (temp < temp_threshold -
 		 msm_thermal_info.temp_hysteresis_degC) {
 		if (limit_idx == limit_idx_high)
 			return;
