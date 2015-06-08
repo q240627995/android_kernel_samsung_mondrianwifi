c122f37c67bb76119c1a0c9e34d6030d3555b1e3
vikrant82 <vikrant82@gmail.com>
2015-06-09 02:48:43 +0530

    Logger compile issue
    Change-Id: I671c021e53c1d78d87eaf0846298938f4394c892

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -37,18 +37,14 @@ static char klog_buf[256];
 #define CONFIG_LOGCAT_SIZE 256
 #endif
 
-static unsigned int log_enabled = 1;
+static unsigned int log_enabled = 0;
 static unsigned int log_always_on = 0;
 
 module_param(log_enabled, uint, S_IWUSR | S_IRUGO);
 module_param(log_always_on, uint, S_IWUSR | S_IRUGO);
 
-static DEFINE_SPINLOCK(log_lock);
 static struct work_struct write_console_wq;
 static struct tasklet_struct schedule_work_tasklet;
-
-static unsigned int log_enabled = 1;
-module_param(log_enabled, uint, S_IWUSR | S_IRUGO);
 
 /*
  * struct logger_log - represents a specific log, such as 'main' or 'radio'
