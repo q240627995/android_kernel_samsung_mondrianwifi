744cbeba648e771cbe42337e09b2318d8e8562c6
Vikrant <Vikrant82@gmail.com>
2015-08-05 01:07:40 +0530

    Revert "crypto: msm: fix memory leak during algorithm registration"
    This reverts commit fbfaf959e3f6bb708cd4b4601ad9ceeaf1f12cab.

--- drivers/crypto/msm/qcrypto.c
+++ drivers/crypto/msm/qcrypto.c
@@ -3959,7 +3959,6 @@ static int  _qcrypto_probe(struct platform_device *pdev)
 				dev_err(&pdev->dev,
 					"The algorithm name %s is too long.\n",
 					q_alg->cipher_alg.cra_name);
-				kfree(q_alg);
 				goto err;
 			}
 		}
@@ -3993,7 +3992,6 @@ static int  _qcrypto_probe(struct platform_device *pdev)
 				dev_err(&pdev->dev,
 					"The algorithm name %s is too long.\n",
 					q_alg->cipher_alg.cra_name);
-				kfree(q_alg);
 				goto err;
 			}
 		}
@@ -4030,7 +4028,6 @@ static int  _qcrypto_probe(struct platform_device *pdev)
 				dev_err(&pdev->dev,
 					"The algorithm name %s is too long.\n",
 					q_alg->sha_alg.halg.base.cra_name);
-				kfree(q_alg);
 				goto err;
 			}
 		}
@@ -4067,7 +4064,6 @@ static int  _qcrypto_probe(struct platform_device *pdev)
 					dev_err(&pdev->dev,
 						"The algorithm name %s is too long.\n",
 						q_alg->cipher_alg.cra_name);
-					kfree(q_alg);
 					goto err;
 				}
 			}
@@ -4106,7 +4102,6 @@ static int  _qcrypto_probe(struct platform_device *pdev)
 					dev_err(&pdev->dev,
 					     "The algorithm name %s is too long.\n",
 					     q_alg->sha_alg.halg.base.cra_name);
-					kfree(q_alg);
 					goto err;
 				}
 			}
@@ -4143,7 +4138,6 @@ static int  _qcrypto_probe(struct platform_device *pdev)
 				dev_err(&pdev->dev,
 						"The algorithm name %s is too long.\n",
 						q_alg->cipher_alg.cra_name);
-				kfree(q_alg);
 				goto err;
 			}
 		}
